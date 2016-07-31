0bbe2f741e79839700d72443d9be3efda4b7fad6
Arthur Saveliev <arthur.saveliev@gmail.com>
2016-07-31 23:37:58 +0200

    - flash view fix

--- demo/src/main/java/com/desmond/demo/FragmentTake.java
+++ demo/src/main/java/com/desmond/demo/FragmentTake.java
@@ -31,7 +31,7 @@ public class FragmentTake extends CameraFragment {
                 if (!hasCameraPermissions()) {
                     requestCameraPermission();
                 }
-                else  ((TestActivity) getActivity()).openGallery();
+                //else  ((TestActivity) getActivity()).openGallery();
             }
         });
     }
